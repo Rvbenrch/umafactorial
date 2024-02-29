550e0e222c22baadef79c8a953c82558f50398d2
Rvbenrch <rubenmanuel2003@gmail.com>
2024-02-29 12:48:26 +0100

    refactorizacion de metodo ded salida

--- refactorizacion.java
+++ refactorizacion.java
@@ -24,8 +24,7 @@ long num = scanInput.nextLong();
 scanInput.close();
 return num;
 }
-public static void main(String []args) {
-long num = getNumber();
+public static void showFac(long num) {
 if (num<0) {
 System.out.println("El factorial no esta definido" + 
 " para numeros negativos");
@@ -33,5 +32,9 @@ System.out.println("El factorial no esta definido" +
 long fac = fact(num);
 System.out.println("El factorial de "+num+" es "+fac);
 }
+}
+public static void main(String []args) {
+long num = getNumber();
+showFac(num);
 }
 >>>>>>> recursivo
